fe1ea0241b8938440b8328b0fb124c67b8e3f8f0
sabrina-fontana <73885980+sabrina-fontana@users.noreply.github.com>
2021-05-10 12:02:10 +0200

    prove con elementi

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import React, {Component} from 'react';
 
+let title = <h2>Primi passi con React</h2>
+function getDate() {
+  return new Date().toLocaleDateString('it-IT') + ' ' + new Date().toLocaleTimeString('it-IT');
+}
+
 class App extends Component {
   render() {
     return (
+      // deve avere un tag contenitore o <React.Fragment>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {title}
+          <p>Oggi è il {getDate()}</p>
         </header>
       </div>
     );
