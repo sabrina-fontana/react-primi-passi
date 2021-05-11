77f28eb1114b5fbf8a66a5c76002fafa73281eff
sabrina-fontana <73885980+sabrina-fontana@users.noreply.github.com>
2021-05-11 11:33:50 +0200

    prove con state

--- src/clock.js
+++ src/clock.js
@@ -1,14 +1,41 @@
 import React from 'react';
 
-function getDate(hours) {
-    // n ore * n secondi * n millisecondi
-    let time = Date.now() + hours*3600*1000;
-    let date = new Date(time);
-    return date.toLocaleDateString('it-IT') + ' ' + date.toLocaleTimeString('it-IT');
-}  
+// props è solo in lettura
+// il component può essere una funzione (più semplice) o una classe (per gestire gli stati)
+// FUNCTION*******************************************************************
+// function clock(props) {
+//     return <p>Timezone {props.timezone}: {getDate(props.timezone)}</p>;
+// }
+// CLASS*******************************************************************
+class Clock extends React.Component {
+    // per accedere alle proprietà passate al component
+    constructor(props) {
+        super(props);
+        // oggetto stato inizializzato nel costruttore
+        // Ogni volta che si modifica una proprietà viene chiamato il metodo render
+        this.state = {
+            date: new Date()
+        }
+    }
 
-function clock(props) {
-    return <p>Timezone {props.timezone}: {getDate(props.timezone)}</p>;
+    render() {
+        // n ore * n secondi * n millisecondi
+        let time = this.state.date.getTime() + this.props.timezone*3600*1000;
+        let date = new Date(time);
+        return <p>Timezone {this.props.timezone}: {date.toLocaleDateString('it-IT')} {date.toLocaleTimeString('it-IT')}</p>;
+    }
+
+    tik = () => this.setState({ 
+        date: new Date() 
+    });
+
+    componentDidMount() {
+        this.interval = setInterval(this.tik, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
 }
 
-export default clock;
+export default Clock;
--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function renderApp() {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-setInterval(renderApp, 1000);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
